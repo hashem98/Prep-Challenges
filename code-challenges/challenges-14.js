11c2a78468fb53376cb26393e27b730cd1ff01d8
hashem98 <hashemsmadi98@gmail.com>
2022-02-03 09:35:46 +0200

    14

---
+++ code-challenges/challenges-14.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// Resource:
+// split: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// slice: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// splice: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+// indexOf: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+// lastIndexOf: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+
+// 1) ---------------------
+// 
+// Using the slice and indexOf/lastIndexOf methods, return the last word in a string
+//
+// Note: consider that the string may have one word but never empty
+//
+// Ex: "I did my waiting 12 years of it in Azkaban" ==> "Azkaban"
+// Ex: "Impossible" ==> "Impossible"
+//
+// ------------------------
+
+const LastWord = (str) => {
+    var n = str.lastIndexOf(" ");
+
+			var res = str.substring(n);
+			return res.trim();
+}
+
+// 2) ---------------------
+// 
+//  Hopefully you struggled in the last code challenge, you can try to do the same task using the split method
+//
+//  EX: "you dare use my spells against me, potter" ===> "potter"
+//
+// ------------------------
+
+const LastWord_2 = (str) => {
+    var n = str.split(" ");
+			return n[n.length - 1];
+}
+
+// 3) ---------------------
+// 
+// Number 2 was easy right?, ok then using the splice method replace all the "I" with "We", "am" with "are" and "was" with "were"
+//
+// Note: you may use another method for search (indexOf)
+//
+// Ex: "I thought you had a plan" ==> "We thought you had a plan"
+// Ex: "I was there 3000 years ago" ==> "We were there 3000 years ago"
+// Ex: "I am Venom" ==> "We are Venom"
+//
+// ------------------------
+
+const replaceWords = (str) => {
+    str = str.split(' ');
+    let word = str.indexOf('I');
+   
+    let wordTwo = str.indexOf('am');
+   
+    let wordThree = str.indexOf('was');
+   if(word != -1){ 
+    str.splice(word,1,'We');
+   }
+   if(wordTwo != -1){ 
+    str.splice(wordTwo,1,'are');
+   }
+   if(wordThree != -1){ 
+    str.splice(wordThree,1,'were');
+   }
+   let text = str.toString();
+   text = text.replace(/,/g, ' ');
+   
+       return text;
+}
+
+// 4) ---------------------
+// 
+// Write a function that "joins" the array of words together and put a comma "," after every five words
+// ["move","it","away","from","the","street"] ==> "move it away from the, street" 
+//
+// ------------------------
+
+const arrToStr = (arr) => {
+    let s , s2;
+    s = arr.splice(5, 0, ',');   
+    s = arr.join(' ');
+    s2 = s.replace(/ \,/g, ',');
+
+       return s2;
+}
+
+// 5) ---------------------
+// 
+// Simon got a string manipulation question for his interview, it asked him to replace the duplicated letters in a string to an indicator and counter
+// as in the example:
+//
+// "aaaa bbb sdf" ===> "a4 b3 s1d1f1"
+//  "door" ==> "d1o2r1"
+//
+// help him by writing the function
+//
+// ------------------------
+
+
+const letterCounter = (str) => {
+    
+    
+	}
+
+
+
+
+module.exports = { LastWord, LastWord_2, replaceWords, arrToStr, letterCounter };
